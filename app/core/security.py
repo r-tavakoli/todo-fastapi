db19436986a3a207cb82a34135bf331e8414a301
r-tavakoli <rah_tav@yahoo.com>
2026-07-04 16:32:58 +0330

    HTTPBearer implemented only for learning

--- app/core/security.py
+++ app/core/security.py
@@ -1,3 +1,25 @@
-from fastapi.security import OAuth2PasswordBearer
+from typing import Annotated
+
+from fastapi import Depends
+from fastapi.security import OAuth2PasswordBearer, HTTPBearer
+from app.core.exceptions import NotAuthenticatedException
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl='/api/v1/users/token')
+
+
+# no using this just for knowing how it works, i wrote this part
+# class TokenBearer(HTTPBearer):
+#     async def __call__(self, request):
+#         auth_credentials = await super().__call__(request)
+        
+#         token = auth_credentials.credentials
+        
+#         if token is None:
+#             raise NotAuthenticatedException()
+        
+#         return token
+    
+    
+# access_token_bearer = TokenBearer()
+
+# token_bearer = Annotated[dict, Depends(access_token_bearer)]
