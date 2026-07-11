addb5365db8005a44dafa956446212a416c78fef
r-tavakoli <rah_tav@yahoo.com>
2026-07-11 18:27:18 +0330

    added tag table through alembic

--- app/models/tasks.py
+++ app/models/tasks.py
@@ -1,6 +1,15 @@
-from sqlmodel import Field, Relationship
+from typing import List
+from sqlmodel import Field, Relationship, SQLModel
 from datetime import datetime
 from .base import BaseModel, TODO_SCHEMA
+
+
+class TaskTag(SQLModel, table=True):
+    __tablename__ = "task_tag"
+    __table_args__ = {"schema": TODO_SCHEMA}
+    
+    task_id: int = Field(foreign_key=f"{TODO_SCHEMA}.task.id", primary_key=True)
+    tag_id: int = Field(foreign_key=f"{TODO_SCHEMA}.tag.id", primary_key=True)
 
 
 class Task(BaseModel, table=True):
@@ -11,6 +20,11 @@ class Task(BaseModel, table=True):
     status_id: int = Field(foreign_key=f"{TODO_SCHEMA}.status.id", default=1)
     priority_id: int = Field(foreign_key=f"{TODO_SCHEMA}.priority.id", default=1)
     due_date_time: datetime
+    tags: List["Tag"] = Relationship(
+        back_populates="tasks",
+        link_model=TaskTag,
+        sa_relationship_kwargs={"lazy": "selectin"}
+    )
     is_deleted: bool = Field(default=False)
     user_id: int = Field(foreign_key=f"{TODO_SCHEMA}.user.id")
     
@@ -33,3 +47,14 @@ class Priority(BaseModel, table=True):
 
     title: str = Field(max_length=50, min_length=3)
     is_deleted: bool = Field(default=False)
+    
+class Tag(BaseModel, table=True):
+    __tablename__ = "tag"
+    __table_args__ = {"schema": TODO_SCHEMA}
+    
+    id: int = Field(default=None, primary_key=True)
+    name: str = Field(unique=True, index=True, max_length=50)
+    is_deleted: bool = Field(default=False)
+    
+    tasks: List["Task"] = Relationship(back_populates="tags", link_model=TaskTag)
+    
